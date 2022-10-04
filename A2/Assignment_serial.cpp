08b4f3386c1a00a76ec0393fc91af2ff3ff1c7c4
isaiahsimeone <isaiahsimeone@gmail.com>
2022-10-04 14:04:21 +1000

    Assignment 2 structure

---
+++ A2/Assignment_serial.cpp
@@ -0,0 +1,134 @@
+// COSC3500, Semester 2, 2022
+// Main file - serial version
+
+#include "eigensolver.h"
+#include "randutil.h"
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#define FILE_DUMP 0 // Should this program write results to a file?
+
+using namespace std;
+
+// global variables to store the matrix
+
+ofstream out_serial;
+double* M = nullptr;
+int N = 0;
+
+// implementation of the matrix-vector multiply function
+void MatrixVectorMultiply(double* Y, const double* X)
+{
+   for (int i = 0; i < N; ++i)
+   {
+      double y = 0;
+      for (int j = 0; j < N; ++j)
+      {
+         y += M[i*N+j] * X[j];
+         //std::cout << "y += " << M[i*N+j] << " * "<< X[j] << std::endl;
+      }
+      Y[i] = y;
+   }
+
+   // Export Y to check validity
+   #if FILE_DUMP
+   for (int i = 0; i < N; i++)
+      out_serial << Y[i] << " ";
+   out_serial << "\n";
+   #endif
+
+}
+
+/*
+void print_matrix() {
+   for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+         std::cout << M[i*N+j] << " ";
+      }
+      std::cout << std::endl;
+   }
+   std::cout << std::endl;
+}
+
+void print_matrix(double* y) {
+   for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+         std::cout << (int)y[i*N+j] << " ";
+      }
+      std::cout << std::endl;
+   }
+   std::cout << std::endl;
+}
+*/
+
+
+int main(int argc, char** argv)
+{
+   // Output file
+   #if FILE_DUMP
+   out_serial.open("serial_results.txt", ios::out );
+   #endif
+
+   // get the current time, for benchmarking
+   auto StartTime = std::chrono::high_resolution_clock::now();
+
+   // get the input size from the command line
+   if (argc < 2)
+   {
+      std::cerr << "expected: matrix size <N>\n";
+      return 1;
+   }
+   N = std::stoi(argv[1]);
+
+   // Allocate memory for the matrix
+   M = static_cast<double*>(malloc(N*N*sizeof(double)));
+
+   // seed the random number generator to a known state
+   randutil::seed(4);  // The standard random number.  https://xkcd.com/221/
+
+   // Initialize the matrix.  This is a matrix from a Gaussian Orthogonal Ensemble.
+   // The matrix is symmetric.
+   // The diagonal entries are gaussian distributed with variance 2.
+   // The off-diagonal entries are gaussian distributed with variance 1.
+   for (int i = 0; i < N; ++i)
+   {
+      M[i*N+i] = std::sqrt(2.0) * randutil::randn();
+      for (int j = i+1; j < N; ++j)
+      {
+         M[i*N + j] = M[j*N + i] = randutil::randn();
+      }
+   }
+
+   //print_matrix(M);
+
+   auto FinishInitialization = std::chrono::high_resolution_clock::now();
+
+   // Call the eigensolver
+   EigensolverInfo Info = eigenvalues_arpack(N, 100);
+
+   auto FinishTime = std::chrono::high_resolution_clock::now();
+
+   // Close file
+   #if FILE_DUMP
+   out_serial.close();
+   #endif
+
+   auto InitializationTime = std::chrono::duration_cast<std::chrono::microseconds>(FinishInitialization - StartTime);
+   auto TotalTime = std::chrono::duration_cast<std::chrono::microseconds>(FinishTime - StartTime);
+
+   std::cout << "Obtained " << Info.Eigenvalues.size() << " eigenvalues.\n";
+   std::cout << "The largest eigenvalue is: " << std::setw(16) << std::setprecision(12) << Info.Eigenvalues.back() << '\n';
+   std::cout << "Total time:                             " << std::setw(12) << TotalTime.count() << " us\n";
+   std::cout << "Time spent in initialization:           " << std::setw(12) << InitializationTime.count() << " us\n";
+   std::cout << "Time spent in eigensolver:              " << std::setw(12) << Info.TimeInEigensolver.count() << " us\n";
+   std::cout << "   Of which the multiply function used: " << std::setw(12) << Info.TimeInMultiply.count() << " us\n";
+   std::cout << "   And the eigensolver library used:    " << std::setw(12) << (Info.TimeInEigensolver - Info.TimeInMultiply).count() << " us\n";
+   std::cout << "Total serial (initialization + solver): " << std::setw(12) << (TotalTime - Info.TimeInMultiply).count() << " us\n";
+   std::cout << "Number of matrix-vector multiplies:     " << std::setw(12) << Info.NumMultiplies << '\n';
+   std::cout << "Time per matrix-vector multiplication:  " << std::setw(12) << (Info.TimeInMultiply / Info.NumMultiplies).count() << " us\n";
+
+   // free memory
+   free(M);
+}
---
+++ A2/test.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+ITERATIONS=1000
+TESTCOUNT=$ITERATIONS-4
+
+RED='\033[0;31m'
+BLUE='\033[1;34m'
+PURP='\033[0;35m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+clr_eol=$(tput el)
+
+# Clean
+echo -en "[${CYAN}INFO${NC}] Cleaning..."
+make clean 1>/dev/null
+rm serial_results.txt cuda_results.txt mpi_results.txt >/dev/null 2>&1
+rm -rf test_output 1>/dev/null
+echo -e " ${GREEN}Done${NC}"
+
+# Build
+echo -en "[${CYAN}INFO${NC}] Building all targets..."
+make 1>/dev/null
+echo -e " ${GREEN}Done${NC}"
+
+# Test directory
+echo -en "[${CYAN}INFO${NC}] Creating directory ./test_output"
+mkdir -p test_output;
+echo -e " ${GREEN}Done${NC}"
+
+echo -e "[${CYAN}INFO${NC}] Running tests with matrix sizes 4 to ${ITERATIONS}..."
+echo ""
+
+# Run Serial tests
+echo -e "------------------------"
+echo -e "                 ${YELLOW}Serial${NC} "
+echo -e "------------------------"
+
+for i in $(seq 4 $ITERATIONS);
+do
+    ./Assignment_serial $i 1>/dev/null
+    cp serial_results.txt test_output/serial_${i}.txt
+    if [[ $i -le $ITERATIONS ]]; then
+        echo -en "${clr_eol}$i/${ITERATIONS} complete\r"
+    fi;
+done;
+echo ""
+echo ""
+
+
+# Run CUDA tests
+echo -e "------------------------"
+echo -e "                    ${YELLOW}CUDA${NC} "
+echo -e "------------------------"
+
+for i in $(seq 4 $ITERATIONS);
+do
+    ./Assignment_cuda $i 1>/dev/null
+    cp cuda_results.txt test_output/cuda_${i}.txt
+    if [[ $i -le $ITERATIONS ]]; then
+        echo -en "${clr_eol}$i/${ITERATIONS} complete\r"
+    fi;
+done;
+echo ""
+echo ""
+
+
+# Run mpi tests
+echo -e "------------------------"
+echo -e "                 ${YELLOW}mpi${NC} "
+echo -e "------------------------"
+
+for i in $(seq 4 $ITERATIONS);
+do
+    ./Assignment_mpi $i 1>/dev/null
+    cp mpi_results.txt test_output/mpi_${i}.txt
+    if [[ $i -le $ITERATIONS ]]; then
+        echo -en "${clr_eol}$i/${ITERATIONS} complete\r"
+    fi;
+done;
+echo ""
+
+# Compare the results
+TOTALFAILURES=0
+
+# CUDA and serial
+echo -e ""
+echo -e "[${CYAN}INFO${NC}] Results generated. Comparing..."
+echo -e ""
+
+echo -e "------------------------"
+echo -e "           ${YELLOW}Serial & CUDA${NC} "
+echo -e "------------------------"
+echo -e ""
+
+FAILURES=0
+for i in $(seq 4 $ITERATIONS);
+do
+    if [[ $i -le $ITERATIONS ]]; then
+        echo -en "${clr_eol}$FAILURES Failures so far. Comparing cuda_${i}.txt and serial_${i}.txt\r"
+    fi;
+    cmp  test_output/serial_${i}.txt test_output/cuda_${i}.txt
+    if [[ $? -ne 0 ]]; then
+        FAILURES=$((FAILURES+1))
+    fi;
+done;
+echo ""
+echo ""
+
+TOTALFAILURES=$FAILURES
+
+# Compare mpi and serial
+echo -e "------------------------"
+echo -e "        ${YELLOW}Serial & mpi${NC} "
+echo -e "------------------------"
+echo -e ""
+
+FAILURES=0
+for i in $(seq 4 $ITERATIONS);
+do
+    if [[ $i -le $ITERATIONS ]]; then
+        echo -en "${clr_eol}$FAILURES Failures so far. Comparing mpi_${i}.txt and serial_${i}.txt\r"
+    fi;
+    cmp  test_output/serial_${i}.txt test_output/mpi_${i}.txt
+    if [[ $? -ne 0 ]]; then
+        FAILURES=$((FAILURES+1))
+    fi;
+done;
+echo ""
+echo ""
+
+TOTALFAILURES=$((TOTALFAILURES+FAILURES))
+
+if [[ $TOTALFAILURES -eq 0 ]]; then
+    echo -e "${GREEN}ALL TESTS SUCCESSFUL${NC}"
+else
+    echo -e "${RED}$TOTALFAILURES total failures encountered.${NC}"
+fi
+
+echo ""
